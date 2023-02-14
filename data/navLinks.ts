bd92761948d6d2bf2f87d803dfa480923851694f
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-02-14 14:55:05 +0100

    refactor: remove unused links

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,27 +1,11 @@
 const navLinks = {
   header: [
     {
-      href: '/blog',
-      label: 'Blog'
-    },
-    {
-      href: '/projects',
-      label: 'Projects'
-    },
-    {
-      href: '/helpers',
-      label: 'Helpers'
-    },
-    {
       href: '/contact',
       label: 'Contact'
     }
   ],
   footer: [
-    {
-      href: '/toolbox',
-      label: 'Toolbox'
-    },
     {
       href: '/resume.pdf',
       label: 'Resume',
