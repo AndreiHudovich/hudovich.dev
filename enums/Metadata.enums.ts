099cb3aa44c89161ef708d014e771da8c86e5061
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-02-15 00:50:04 +0100

    fix: update social image path

--- enums/Metadata.enums.ts
+++ enums/Metadata.enums.ts
@@ -1,6 +1,6 @@
 enum Metadata {
   BASE_URL = 'https://hudovich.dev/',
-  SOCIAL_WEBSITE_LOGO = 'https://hudovich.dev/social-website-logo.png',
+  SOCIAL_WEBSITE_LOGO = 'https://hudovich.dev/images/social-website-logo.png',
   TWITTER_USERNAME = '@andrei_hudovich',
   TWITTER_URL = 'https://twitter.com/andrei_hudovich',
   GITHUB_URL = 'https://github.com/andrei-hudovich'
