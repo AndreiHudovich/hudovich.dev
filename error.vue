58d5fe39f667afec0b9203d10135220b24559624
Andrei Hudovich <andrei.hudovich@gmail.com>
2023-02-15 23:37:04 +0100

    fix: fix error page #4

--- error.vue
+++ error.vue
@@ -33,13 +33,12 @@
 const error = useError()
 
 const is404Error = computed(() => {
-  if (error?.value && !(error.value instanceof Error)) {
-    return error.value.statusCode === '404'
+  if (error.value && !(error.value instanceof Error)) {
+    return String(error.value.statusCode) === '404'
   }
 })
 
-console.dir(error.value)
-console.log(`is404Error: ${is404Error.value}`)
+console.log(`is 404: ${is404Error.value}`)
 
 const handleError = () => clearError({ redirect: '/' })
 </script>
