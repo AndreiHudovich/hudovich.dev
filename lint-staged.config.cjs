9a71fdd2637d2d1a2cd2d897c8aa03ea0189793d
Andrei Hudovich <andrei.hudovich@gmail.com>
2024-03-23 23:04:55 +0100

    chore: clean slate

--- lint-staged.config.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.{ts,astro}': [() => 'npm run lint:tsc'],
-  '**/*.{js,ts,astro}': ['npm run lint:eslint', 'npm run lint:prettier']
-}
--- src/enums/FileNames.enum.ts
+++
@@ -1,5 +0,0 @@
-enum FileNames {
-  RESUME = 'Andrei_Hudovich_-_Senior_Front-end_Engineer_-_Resume.pdf'
-}
-
-export default FileNames
