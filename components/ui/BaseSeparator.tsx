74bafbc05434381887347db4d642a25a04fe8b94
Andrei Hudovich <andrei.hudovich@gmail.com>
2024-05-30 18:52:55 +0200

    feat: add hiking page

--- components/ui/BaseSeparator.tsx
+++ components/ui/BaseSeparator.tsx
@@ -4,8 +4,8 @@ import type { ComponentPropsWithoutRef } from 'react'
 
 function BaseSeparator({ className, ...props }: ComponentPropsWithoutRef<'svg'>) {
   return (
-    <div className="py-1" role="none">
-      <Separator className={cn('w-[8.625rem] h-3 fill-orange-500', className)} {...props} />
+    <div className={cn('py-1', className)} role="none">
+      <Separator className="w-[8.625rem] h-3 fill-orange-500" {...props} />
     </div>
   )
 }
