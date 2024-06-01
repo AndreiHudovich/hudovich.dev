d7fe0981002869715fc0d8063fccc8ef3f0257da
Andrei Hudovich <andrei.hudovich@gmail.com>
2024-06-01 16:48:54 +0200

    feat: add GreetingLog component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import { Outfit } from 'next/font/google'
+import GreetingLog from '@/components/GreetingLog.tsx'
 import Footer from '@/components/layout/Footer.tsx'
 import Header from '@/components/layout/Header.tsx'
 import { cn } from '@/utils/css.ts'
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={cn(outfit.variable, 'h-full')} suppressHydrationWarning={true}>
       <body className="h-full min-h-full bg-white font-[350] text-dark-blue-950 antialiased dark:bg-blue-950 dark:text-slate-300">
         <ThemeProvider disableTransitionOnChange={true}>
+          <GreetingLog />
           <Header />
           <main className="pt-8 pb-16 md:pt-16 md:pb-24">{children}</main>
           <Footer />
