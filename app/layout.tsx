e96876ed719d09914ef891a6d20c444784bf190e
Andrei Hudovich <--global>
2024-06-26 22:33:43 +0200

    chore: disable analytics for myself

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <main className="pt-8 pb-16 md:pt-16 md:pb-24">{children}</main>
           <Footer />
         </ThemeProvider>
-        <Analytics  />
+        <Analytics beforeSend={(event) => (localStorage.getItem('va-disable') ? null : event)} />
         <SpeedInsights />
       </body>
     </html>
